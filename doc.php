f42b0d6efeb18d703c1100b60139ce63ef41d404
kellerflint <kflint0068@gmail.com>
2020-01-06 10:20:24 -0800

    commit

---
+++ doc.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $students["name"] = 2.0;
+    // ... and so on
+
+
+    // helper function to convert grades in decimal to alphabetic
+    // @param grade - we pass it a single decimal grade value and return the letter grade for it
+    function gradeConverter($grade) {
+        // logic to convert the grade
+
+        // return the value
+        return "A";
+    }
+
+    // FOR EACH student name and PRINT the following on the page
+    
+    // 1. Print the student name
+    // 2. Print the student decimal grade
+    // 3. Print the student letter grade
+
+    ?>
+</body>
+</html>
